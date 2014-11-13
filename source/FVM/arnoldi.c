ac11690bd77084e57a3a0926ed99262269daa8a1
Morgan Wall <mwall1992@gmail.com>
2014-11-13 10:23:15 +1000

    Working implementation of Arnoldi in C (without crashes)

--- source/FVM/arnoldi.c
+++ source/FVM/arnoldi.c
@@ -33,6 +33,10 @@ void copy_array(int n, double* array, double* copy);
 void retrieve_array_from_matrix(int rows, int columns, double* matrix, 
 	double* copy, int index, orientation_t orientation);
 
+double euclidean_norm(int n, double* vector);
+
+double dot_product(int n, double* a, double* b);
+
 /* Implementation */
 
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
@@ -64,12 +68,16 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
 	error_tol = mxGetScalar(prhs[5]);
 
 	// initialise arnoldi parameters
-	mxArray* Q = mxCreateDoubleMatrix((mwSize)m, (mwSize)restart_value, mxREAL);
-	mxArray* H = mxCreateDoubleMatrix((mwSize)(restart_value + 1), (mwSize)restart_value, mxREAL);
-	mxArray* g = mxCreateDoubleMatrix((mwSize)(restart_value + 1), (mwSize)1, mxREAL);
+	mxArray* Q = 
+		mxCreateDoubleMatrix((mwSize)m, (mwSize)(restart_value + 1), mxREAL);
+	mxArray* H = 
+		mxCreateDoubleMatrix((mwSize)(restart_value + 1), (mwSize)restart_value, mxREAL);
+	mxArray* g = 
+		mxCreateDoubleMatrix((mwSize)(restart_value + 1), (mwSize)1, mxREAL);
 
 	arnoldi(A, L, U, b, m, n, restart_value, error_tol, Q, H, g);
 
+	// assign output parameters
 	plhs[0] = Q;
 	plhs[1] = H;
 	plhs[2] = g;
@@ -79,42 +87,38 @@ void arnoldi(mxArray* A, mxArray* L, mxArray* U, double* b,
 	long m, long n, unsigned int restart_value, 
 	unsigned int error_tol, mxArray* Q, mxArray* H, mxArray* g) {
 
-	// initialise mex solver parameters
+	// initialise known constants
+	int const VECTOR_COLUMN_COUNT = 1;
 	int const MEX_OUTPUT_PARMETERS = 1;
 	int const MEX_INPUT_PARAMETERS = 2;
 
-	mxArray *lhs[MEX_OUTPUT_PARMETERS], *rhs[MEX_INPUT_PARAMETERS];
+	long double_size = sizeof(double);
 
-	mxArray* tempMxArray = mxCreateDoubleMatrix(m, 1, mxREAL);
-
+	// parse input parameters
 	double* q_data = mxGetPr(Q);
 	double* h_data = mxGetPr(H);
 	double* g_data = mxGetPr(g);
 
-	// initialise solver parameters
-	long double_size = sizeof(double);
+	// initialise temporary storage
+	mxArray* temp_mxArray_m = 
+		mxCreateDoubleMatrix(m, VECTOR_COLUMN_COUNT, mxREAL);
+	mxArray *lhs[MEX_OUTPUT_PARMETERS], *rhs[MEX_INPUT_PARAMETERS];
+	double* temp_vector_m = mxCalloc(m, double_size);
+	double test;
 
+	// initialise Arnoldi iteration variables
 	double H_diag_temp;
 	double g_temp;
-	double* temp_basis_vector = mxCalloc(m, double_size);
+	double* q = mxCalloc(m, double_size);
 
 	// initialise Given's rotations variables
 	double* c = mxCalloc(restart_value, double_size);
-	double* s = mxCalloc(restart_value, double_size);
+	double* s = mxCalloc(restart_value, double_size);	
 	
 	// generate initial basis vector
-	double* q = mxCalloc(m, double_size);
-	
-	// double b_norm = dnrm2(&m, b, &double_size);
-	double b_norm = 0;
+	g_data[0] = euclidean_norm(m, b);
 	for (int i = 0; i < m; i++) {
-		b_norm = b_norm + pow(b[i], 2);
-	}
-	b_norm = sqrt(b_norm);
-	g_data[0] = b_norm;
-
-	for (int i = 0; i < m; i++) {
-		q_data[i] = b[i] / b_norm; 
+		q_data[i] = b[i] / g_data[0]; 
 	}
 
 	// generate each basis vector in the Krylov subspace
@@ -122,11 +126,11 @@ void arnoldi(mxArray* A, mxArray* L, mxArray* U, double* b,
 
 		// determine the next Kyrlov subspace basis vector
 		retrieve_array_from_matrix(m, restart_value, q_data, 
-			temp_basis_vector, i, COLUMN);
-		mxSetPr(tempMxArray, temp_basis_vector);
+			temp_vector_m, i, COLUMN);
+		mxSetPr(temp_mxArray_m, temp_vector_m);
 
 		rhs[0] = L;
-		rhs[1] = tempMxArray;
+		rhs[1] = temp_mxArray_m;
 
 		mexCallMATLAB(MEX_OUTPUT_PARMETERS, lhs, MEX_INPUT_PARAMETERS, rhs, 
 			"mldivide");
@@ -149,40 +153,25 @@ void arnoldi(mxArray* A, mxArray* L, mxArray* U, double* b,
 		for (unsigned int j = 0; j <= i; j++) {
 
 			retrieve_array_from_matrix(m, restart_value, q_data, 
-				temp_basis_vector, j, COLUMN);
-			
-			// h_data[i * H_ROWS + j] = 
-			// 	ddot(&m, temp_basis_vector, &double_size, q, &double_size);
-			h_data[i * H_ROWS + j] = 0;
-			for (int k = 0; k < m; k++) {
-				h_data[i * H_ROWS + j] = h_data[i * H_ROWS + j] + temp_basis_vector[k] * q[k];
-			}
+				temp_vector_m, j, COLUMN);
+
+			h_data[i * H_ROWS + j] = dot_product(m, temp_vector_m, q);
 
 			retrieve_array_from_matrix(Q_ROWS, restart_value, q_data, 
-				temp_basis_vector, j, COLUMN);
+				temp_vector_m, j, COLUMN);
 
-			// dscal(&m, &h_data[i * H_ROWS + j], temp_basis_vector, &double_size);
 			for (int k = 0; k < m; k++) {
-				temp_basis_vector[k] = temp_basis_vector[k] * h_data[i * H_ROWS + j];
+				temp_vector_m[k] = temp_vector_m[k] * h_data[i * H_ROWS + j];
 			}
 
 			for (int k = 0; k < m; k++) {
-				q[k] = q[k] - temp_basis_vector[k];
+				q[k] = q[k] - temp_vector_m[k];
 			}
 		}
 
 		// normalise the basis vector
-		// h_data[i * H_ROWS + (i+1)] = dnrm2(&m, q, &double_size);
-		h_data[i * H_ROWS + (i+1)] = 0;
-		for (int j = 0; j < m; j++) {
-			h_data[i * H_ROWS + (i+1)] = h_data[i * H_ROWS + (i+1)] + pow(q[j], 2);
-		}
-		h_data[i * H_ROWS + (i+1)] = sqrt(h_data[i * H_ROWS + (i+1)]);
-
-		// for (int j = 0; j < m; j++) {
-		// 	q_data[(i + 1) * Q_ROWS + j] = q[i] / h_data[i * H_ROWS + (i+1)];
-		// }
-
+		h_data[i * H_ROWS + (i+1)] = euclidean_norm(m, q);
+		
 		for (int j = 0; j < m; j++) {
 			q_data[(i + 1) * Q_ROWS + j] = q[j] / h_data[i * H_ROWS + (i+1)];
 		}
@@ -212,37 +201,17 @@ void arnoldi(mxArray* A, mxArray* L, mxArray* U, double* b,
 		g_data[i] = c[i] * g_temp;
 		g_data[i+1] = - s[i] * g_temp;
 
-		if (abs(g_data[i+1]) < error_tol || i == restart_value) {
+		if (abs(g_data[i+1]) < error_tol || i == restart_value - 1) {
+			mxSetN(Q, i+1);
+			mxSetM(H, i+2);
+			mxSetN(H, i+1);
+			mxSetM(g, i+2);
 			break;
 		}
-
-		// if (abs((*g)[i+1]) < error_tol || i == restart_value) {
-
-		// 	// // reallocate basis vectors based on the number of Arnoldi iterations
-		// 	// for (int j = 0; j < m; j++) {
-		// 	// 	(*Q)[j] = realloc((*Q)[j], sizeof(***Q) * i);
-		// 	// }
-
-		// 	// // reallocate Hessianberg matrix based on the number of 
-		// 	// // Arnoldi iterations
-		// 	// for (int j = 0; j < restart_value + 1; j++) {
-		// 	// 	if (j > i + 1) {
-		// 	// 		free((*H)[j]);
-		// 	// 	} else {
-		// 	// 		(*H)[j] = realloc((*H)[j], sizeof(***H) * i);
-		// 	// 	}
-		// 	// }
-		// 	// realloc((*H), sizeof(**H) * (i + 1));
-
-		// 	// // reallocate residual based on the number of Arnoldi iterations
-		// 	// g = realloc(g, sizeof(*g) * (i+1));
-
-		// 	break;
-		// }
 	}
 
 	// deallocate solver variables
-	// mxFree(temp_basis_vector);
+	// mxFree(temp_vector_m);
 	// mxFree(c);
 	// mxFree(s);
 }
@@ -267,4 +236,21 @@ void retrieve_array_from_matrix(int rows, int columns, double* matrix,
 			copy[j] = matrix[i];
 		}
 	}
+}
+
+double euclidean_norm(int n, double* vector) {
+	double norm = 0;
+	for (int i = 0; i < n; i++) {
+		norm += pow(vector[i], 2);
+	}
+	norm = sqrt(norm);
+	return norm;
+}
+
+double dot_product(int n, double* a, double* b) {
+	double product = 0;
+	for (int i = 0; i < n; i++) {
+		product += a[i] * b[i];
+	}
+	return product;
 }
